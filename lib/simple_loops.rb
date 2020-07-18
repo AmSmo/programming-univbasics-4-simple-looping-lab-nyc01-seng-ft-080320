673809611910f877ad72117ef39e16cd43966def
Adam Smolenski <adam.smolenski@gmail.com>
2020-07-18 17:23:57 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,21 @@
 # Write your methods here
+
+def loop_message_fives_times(message)
+  5.times do 
+    puts message
+  end
+end
+
+def loop_message_n_times(message, n)
+  n.times do
+    puts message
+  end
+end
+
+def output_array(array)
+  
+end
+
+def return_string_array(arr)
+  arr.map {|i| i.to_s }
+end
